a14ee7bc82be1cd01aac7137bcd4ad1b5e65f580
EmanSan9 <sanna950@gmail.com>
2025-07-28 16:33:28 +0200

    VFINAL

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -9,11 +9,13 @@ class SingleBook extends Component {
     return (
       <Card
         onClick={() => onSelect(book.asin)}
-        // Quando un SingleBook viene cliccato, chiama onSelect(book.asin).
+        // Quando un SingleBook viene cliccato, chiama onSelect().
 
         // Le passo come argomento book.asin (cioè il valore della proprietà asin del libro corrente).
         style={{
           border: isSelected ? "3px solid red" : "1px solid #ddd",
+          // Quando clicco su una card (<Card>), il book.asin viene passato a onSelect, che aggiorna lo stato selectedBookAsin nel componente padre (BookList).
+          // Questo fa sì che il libro selezionato abbia isSelected === true, e quindi venga evidenziato con il bordo rosso.
           cursor: "pointer",
         }}
       >
